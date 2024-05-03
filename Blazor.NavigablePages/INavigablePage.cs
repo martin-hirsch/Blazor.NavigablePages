37f04bd93b201f3a33ef85da74ed738d8518a70d
Martin Hirsch <mh@jamoin.de>
2024-05-03 21:21:04 +0200

    Initial Commit

---
+++ Blazor.NavigablePages/INavigablePage.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.NavigablePages;
+
+public interface INavigablePage<TRoutableComponent> where TRoutableComponent : INavigablePage<TRoutableComponent>
+{ 
+    static abstract string Route { get; }
+    
+    static void NavigateTo(NavigationManager navigationManager)
+    {
+        navigationManager.NavigateTo(TRoutableComponent.Route);
+    }
+}
+
+public interface INavigablePage<in TRoutableComponent, in TNavigationParameter> where TRoutableComponent : INavigablePage<TRoutableComponent, TNavigationParameter>
+{
+    static abstract string Route { get; }
+    static abstract void NavigateTo(NavigationManager navigationManager, TNavigationParameter navigationParameter);
+}
---
+++ Blazor.NavigablePages/NavigationManagerExtension.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.NavigablePages;
+
+public static class NavigationManagerExtension
+{
+    public static void NavigateTo<TRoutableComponent>(this NavigationManager navigationManager) where TRoutableComponent : INavigablePage<TRoutableComponent>
+    {
+        navigationManager.NavigateTo(TRoutableComponent.Route);
+    }
+
+    public static void NavigateTo<TRoutableComponent, TNavigationParameter>(this NavigationManager navigationManager, TNavigationParameter navigationParameter) where TRoutableComponent : INavigablePage<TRoutableComponent, TNavigationParameter>
+    {
+        TRoutableComponent.NavigateTo(navigationManager, navigationParameter);
+    }
+}
