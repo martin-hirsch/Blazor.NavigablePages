e903748d88d27aa8ad7e705e185ddf79b84dd822
Martin Hirsch <mh@jamoin.de>
2024-08-13 10:29:03 +0200

    refactor: Rename Route to RouteTemplate

--- Blazor.NavigablePages/INavigablePage.cs
+++ Blazor.NavigablePages/INavigablePage.cs
@@ -4,16 +4,16 @@ namespace Blazor.NavigablePages;
 
 public interface INavigablePage<TRoutableComponent> where TRoutableComponent : INavigablePage<TRoutableComponent>
 { 
-    static abstract string Route { get; }
+    static abstract string RouteTemplate { get; }
     
     static void NavigateTo(NavigationManager navigationManager)
     {
-        navigationManager.NavigateTo(TRoutableComponent.Route);
+        navigationManager.NavigateTo(TRoutableComponent.RouteTemplate);
     }
 }
 
 public interface INavigablePage<in TRoutableComponent, in TNavigationParameter> where TRoutableComponent : INavigablePage<TRoutableComponent, TNavigationParameter>
 {
-    static abstract string Route { get; }
+    static abstract string RouteTemplate { get; }
     static abstract void NavigateTo(NavigationManager navigationManager, TNavigationParameter navigationParameter);
 }
